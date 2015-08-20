443437b4faa9555f1d67384bd53f908d1bbecd14
Fares Droubi <fdr@zenexity.com>
2015-08-20 18:37:33 +0200

    Freatured item Preview.

--- public/javascripts/slices.js
+++ public/javascripts/slices.js
@@ -50,7 +50,21 @@ $(document).ready(function () {
 
     })();
 
-    (function Map() {
+    (function FeaturedItemPreview() {
+
+        function select() {
+            var paneId = $(this).attr('data-paneid');
+            var $previewPane = $(this).parents('.featured-preview').find('.preview-pane');
+            $previewPane.find('.preview-image').css('opacity', 0);
+            $previewPane.find('.preview-image[data-paneid="' + paneId + '"]').css('opacity', 1);
+        }
+
+        $('.featured-preview [data-paneid]').on('mouseenter', select);
+        $('.featured-preview [data-paneid]').first().map(select);
+    })()
+
+
+        (function Map() {
 
         $('.contact-us .map').each(function() {
 
