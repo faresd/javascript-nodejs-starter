f718898eeb3af9223c0b3fb5da37233825ab3a66
Fares Droubi <fdr@zenexity.com>
2015-08-25 12:50:48 +0200

    Adding index layout, and deleting useless routes.

--- routes/index.js
+++ routes/index.js
@@ -11,28 +11,6 @@ exports.index = prismic.route(function(req, res, ctx) {
   });
 });
 
-// -- Display a given document
-
-exports.detail = prismic.route(function(req, res, ctx) {
-  var id = req.params['id'],
-      slug = req.params['slug'];
-
-  prismic.getDocument(ctx, id, slug,
-    function(err, doc) {
-      if (err) { prismic.onPrismicError(err, req, res); return; }
-      res.render('detail', {
-        doc: doc
-      });
-    },
-    function(doc) {
-      res.redirect(301, ctx.linkResolver(doc));
-
-    },
-    function(NOT_FOUND) {
-      res.send(404, 'Sorry, we cannot find that!');
-    }
-  );
-});
 
 function toCamelcase(name) {
   return name.replace(/-([a-z])/g, function (g) { return g[1].toUpperCase()})
@@ -67,30 +45,6 @@ exports.page = prismic.route(function(req, res, ctx) {
     })
 });
 
-// -- Search in documents
-
-exports.search = prismic.route(function(req, res, ctx) {
-  var q = req.query['q'];
-
-  if(q) {
-    ctx.api.form('everything').set("page", req.param('page') || "1").ref(ctx.ref)
-           .query('[[:d = fulltext(document, "' + q + '")]]').submit(function(err, docs) {
-      if (err) { prismic.onPrismicError(err, req, res); return; }
-      res.render('search', {
-        q: q,
-        docs: docs,
-        url: req.url
-      });
-    });
-  } else {
-    res.render('search', {
-      q: q,
-      docs: null,
-      url: req.url
-    });
-  }
-
-});
 
 // -- Preview documents from the Writing Room
 
